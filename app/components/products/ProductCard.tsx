1b37ad037faa6533e41efabbebf8dca0bd3948c3
arafathsiam99 <siammdarafathislam@gmail.com>
2024-10-01 16:58:40 +0600

    product details added

--- app/components/products/ProductCard.tsx
+++ app/components/products/ProductCard.tsx
@@ -2,15 +2,22 @@
 
 import { formatPrice } from "@/utils/formatPrice";
 import { truncateText } from "@/utils/truncateText";
+import { Rating } from "@mui/material";
 import Image from "next/image";
+import { useRouter } from "next/navigation";
 
 interface ProductCardProps {
   data: any;
 }
 
 const ProductCard: React.FC<ProductCardProps> = ({ data }) => {
+  const router = useRouter();
+  const productRating =
+    data.reviews.reduce((acc: number, item: any) => item.rating + acc, 0) /
+    data.reviews.length;
   return (
     <div
+      onClick={() => router.push(`/product/${data.id}`)}
       className="col-span-1
     cursor-pointer
     border-[1.2px]
@@ -40,7 +47,9 @@ const ProductCard: React.FC<ProductCardProps> = ({ data }) => {
           />
         </div>
         <div className="mt-4">{truncateText(data.name)}</div>
-        <div></div>
+        <div>
+          <Rating value={productRating} readOnly />
+        </div>
         <div>{data.reviews.length} reviews</div>
         <div className="font-semibold">{formatPrice(data.price)}</div>
       </div>
